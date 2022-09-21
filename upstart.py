2a3dbccbddcbe43f7d63e0558c7453a50ba88b1e
Patrick Sidney <patori@gmail.com>
2022-09-21 19:42:14 -0400

    Update upstart.py

--- upstart.py
+++ upstart.py
@@ -1,7 +1,7 @@
 import requests
 import time
 
-def get_price():
+def get_rate():
 
     url = "https://www.google.com/async/finance_wholepage_price_updates?ei=gPcoY-2_NbHJptQP-a6AqAE&rlz=1C1CHZN_enUS942US942&yv=3&cs=0&async=mids:%2Fg%2F11lky30jcs%7C%2Fm%2F016yss%7C%2Fm%2F02853rl%7C%2Fm%2F04zvfw%7C%2Fg%2F1q52gbb7v,currencies:,_fmt:jspb"
 
